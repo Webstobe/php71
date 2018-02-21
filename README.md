943107b6f779285ce2ea66928b40cf0e59263507
Nando Bosshart <nando@bosshartong.ch>
2018-02-21 17:58:52 +0100

    PHP 7.1

---
+++ README.md
@@ -0,0 +1,15 @@
+# php71
+
+overview
+------------
+- based on php:7.1-apache
+- composer from official image
+- additional tools:
+  - wget
+  - nano
+  - vim
+  - git
+  - unzip / zip
+  - rsync
+  - graphicsmagick
+  - mysql-client
---
+++ entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# change directory:
+cd /var/www
+
+# only if /typo3-folder is not already present:
+if  [ ! -d "/var/www/web/typo3" ];then
+    echo -e "==================================="
+    echo -e "== PREPARING INITIAL TYPO3-SETUP =="
+    echo -e "==================================="
+    # run composer update
+    composer update
+
+    # run TYPO3-setup:
+    typo3cms install:setup --no-interaction --force --skip-extension-setup=true --database-user-name=dev --database-user-password=dev --database-host-name=mysql --database-port=3306 --database-name=typo3 --admin-user-name='admin' --admin-password='password' --site-name='Docker' --database-create=0 --use-existing-database  --site-setup-type=no
+
+    # restore DB:
+    typo3cms database:import < /var/www/dump-live.sql
+
+    # generate packagestates including core-extensions specified in composer.json:
+    typo3cms install:generatepackagestates
+
+    # typo3console extension:setupactive
+    typo3cms extension:setupactive
+
+    # check the DB-scheme
+    typo3cms database:updateschema
+
+    # fix folder permissions
+    typo3cms install:fixfolderstructure
+
+    # finally cache:flush
+    typo3cms cache:flush
+
+fi
+
+# chown /var/www:
+chown -R www-data:www-data /var/www
+
+echo -e "==================================="
+echo -e "==      CONTAINER IS READY       =="
+echo -e "==================================="
+
+exec "$@"
+#/bin/bash
