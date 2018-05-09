5b4aff9f7af6eab8141cefffad3c29e5c818b226
Nando Bosshart <nando@bosshartong.ch>
2018-05-09 17:46:19 +0200

    - update entrypoint-script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# change directory:
+# change directory:
 cd /var/www
 
 # only if /typo3-folder is not already present:
@@ -15,7 +15,7 @@ if  [ ! -d "/var/www/web/typo3" ];then
     typo3cms install:setup --no-interaction --force --skip-extension-setup=true --database-user-name=dev --database-user-password=dev --database-host-name=mysql --database-port=3306 --database-name=typo3 --admin-user-name='admin' --admin-password='password' --site-name='Docker' --database-create=0 --use-existing-database  --site-setup-type=no
 
     # restore DB:
-    typo3cms database:import < /var/www/dump-live.sql
+    typo3cms database:import < /var/www/ingredients/mysql/initialdump.sql
 
     # generate packagestates including core-extensions specified in composer.json:
     typo3cms install:generatepackagestates
@@ -29,9 +29,20 @@ if  [ ! -d "/var/www/web/typo3" ];then
     # fix folder permissions
     typo3cms install:fixfolderstructure
 
+    # install DE-language
+    typo3cms language:update de
+
     # finally cache:flush
     typo3cms cache:flush
 
+fi
+
+if  [ ! -d "/var/www/web/fileadmin/user_upload/test_files" ];then
+    echo -e "============================"
+    echo -e "== initializing fileadmin =="
+    echo -e "============================"
+    cp -r /var/www/ingredients/fileadmin/user_upload/test_files /var/www/web/fileadmin/user_upload/test_files/
+    cp -r /var/www/ingredients/fileadmin/user_upload/logos /var/www/web/fileadmin/user_upload/logos/
 fi
 
 # chown /var/www:
